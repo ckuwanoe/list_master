20f9dc5155b6d3b9f258e75009282c17ea7eaaf3
Corey Kuwanoe <corey.kuwanoe@gmail.com>
2012-10-15 15:54:39 -0700

    adding devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121014193202) do
+ActiveRecord::Schema.define(:version => 20121015225225) do
 
   create_table "assets", :force => true do |t|
     t.integer  "list_id"
@@ -61,6 +61,29 @@ ActiveRecord::Schema.define(:version => 20121014193202) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.integer  "role_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "state"
+    t.integer  "region_id",              :default => 0
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
   create_table "walked_packets", :force => true do |t|
     t.integer  "packet_id"
