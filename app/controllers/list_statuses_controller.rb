3f57d62fedc5f8f164ca33cb6584bbfc639b5d40
Corey Kuwanoe <corey.kuwanoe@gmail.com>
2012-10-16 00:43:51 -0700

    adding new list status

--- app/controllers/list_statuses_controller.rb
+++ app/controllers/list_statuses_controller.rb
@@ -2,11 +2,11 @@ class ListStatusesController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-
+    @lists = List.region_and_status_join.order("precincts.county, precincts.precinct_number")
   end
 
   def new
-
+    @lists = List.region_and_status_join.order("precincts.county, precincts.precinct_number")
   end
 
   def create
