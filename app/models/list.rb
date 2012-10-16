0dbd6f6f1e1dc776c43f06251524bc985a780cbe
Corey Kuwanoe <corey.kuwanoe@gmail.com>
2012-10-15 21:24:34 -0700

    adding latest status to list

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,11 +2,15 @@ class List < ActiveRecord::Base
   attr_accessible :list_name, :van_list_id, :van_url, :precinct_id
   has_many :list_statuses
   has_many :organizations, :through => :list_statuses
-  has_one :precinct
+  belongs_to :precinct
 
   def self.region_and_status_join
     joins("INNER JOIN precincts ON lists.precinct_id = precincts.id INNER JOIN teams ON precincts.team_id = teams.id
       INNER JOIN organizers ON teams.organizer_id = organizers.id INNER JOIN regions ON organizers.region_id = regions.id
-      INNER JOIN list_statuses ON lists.id = list_statuses.list_id")
+      LEFT OUTER JOIN list_statuses ON lists.id = list_statuses.list_id")
+  end
+
+  def latest_status
+    status = ListStatus.where(:list_id => self.id).order("created_at DESC").first
   end
 end
