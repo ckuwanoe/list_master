9bec2647b6ce13149dd41589563bfdb97375b924
Corey Kuwanoe <corey.kuwanoe@gmail.com>
2012-10-15 19:40:59 -0700

    fixing join sql

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,7 @@
 class ListsController < ApplicationController
   before_filter :authenticate_user!
   def index
-    @lists = List.region_and_status_join.order("precincts.county, precincts.prec")
+    @lists = List.region_and_status_join.order("precincts.county, precincts.precinct_number")
   end
 
 end
