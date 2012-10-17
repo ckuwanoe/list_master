353ff074e06bf6e3cff3bcb2bed881b004a007b4
Corey Kuwanoe <corey.kuwanoe@gmail.com>
2012-10-16 18:34:19 -0700

    adding form to update lists

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def date_to_db(date)
+    if date.match(/\//)
+      return db_date = Date.strptime(date, "%m/%d/%Y").to_s("%Y-%m-%d")
+    else
+      return date
+    end
+  end
 end
--- app/controllers/list_statuses_controller.rb
+++ app/controllers/list_statuses_controller.rb
@@ -7,9 +7,21 @@ class ListStatusesController < ApplicationController
 
   def new
     @lists = List.region_and_status_join.order("precincts.county, precincts.precinct_number")
+    @list_status = ListStatus.new
   end
 
   def create
-
+    params[:list_status][:date] = date_to_db(params[:list_status][:date])
+    @list_status = ListStatus.new(params[:list_status])
+    respond_to do |format|
+      if @list_status.save
+        flash[:success] = 'Successfully entered in numbers'
+        format.html { redirect_to lists_path, notice: ''}
+      else
+        flash[:error] = "Cannot add numbers. Please check your numbers and try again."
+        format.html { render action: "new" }
+        format.json { render json: @list_status.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
